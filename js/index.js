33ec4feb72d5ceb4eb3e364a26c7176b857c4add
jacobdurst <jdd2007@gmail.com>
2019-05-04 15:59:27 -0400

    finally fixed flexbox for good at desktop and mobile sizes

--- js/index.js
+++ js/index.js
@@ -10,3 +10,19 @@ event.target.style.color = "yellow";
     event.target.style.color = "";
   }, 500);
 }, false);
+
+
+
+const bannerOverlay = document.querySelector(".overlay");
+
+bannerOverlay.addEventListener("mouseenter", (event) => {
+    event.target.style.boxShadow = "0px 0px 20px 10px rgba(0, 0, 0, 0.5)";
+    event.target.style.transition = "box-shadow 0.1s ease-in-out";
+    event.stopPropagation();
+});
+
+bannerOverlay.addEventListener('mouseleave', (event) => {
+    event.target.style.boxShadow = "0px 0px 10px 5px rgba(0, 0, 0, 0.50)";
+    event.target.style.transition = "box-shadow 0.3s ease-in-out";
+    event.stopPropagation();
+});
